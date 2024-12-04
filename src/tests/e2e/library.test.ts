3a8784ec62d1712dc912029c3332763a5e099e8e
Miguel A. Gómez <software.crafters.sl@gmail.com>
2024-12-04 16:37:36 +0000

    refactor: introduce variable and extract functions

--- src/tests/e2e/library.test.ts
+++ src/tests/e2e/library.test.ts
@@ -16,54 +16,49 @@ describe('Library App', () => {
   });
 
   it('should be able to delete a book', ()=>{
-    cy.get('[data-test-id="titleInput"]').type('New book');
-    cy.get('[data-test-id="coverInput"]').type('https://bucket.mlcdn.com/a/1590/1590228/images/75a5707709691e7651c5ebcace5287da35c56015.png');
+    const aBook = 'New book';
+    addBook(aBook);
 
-    cy.get('[data-test-id="addButton"]').click();
+    deleteBook();
 
-    cy.get('[data-test-id="deleteButton"]').click();
-    cy.contains('[data-test-id="bookList"]', 'New book').should('not.exist')
+    cy.contains('[data-test-id="bookList"]', aBook).should('not.exist')
   });
 
   it('should be able to mark as read a book', ()=>{
-    cy.get('[data-test-id="titleInput"]').type('New book');
-    cy.get('[data-test-id="coverInput"]').type('https://bucket.mlcdn.com/a/1590/1590228/images/75a5707709691e7651c5ebcace5287da35c56015.png');
-    cy.get('[data-test-id="addButton"]').click();
+    const aBook = 'New book';
+    addBook(aBook)
 
     cy.get('[data-test-id="markAsReadButton"]').click();
 
     cy.get('[data-test-id="markAsReadIcon"]').should('exist');
-    cy.get('[data-test-id="deleteButton"]').click();
+    deleteBook();
   });
 
   it('should be able to filter as read', ()=>{
-    cy.get('[data-test-id="titleInput"]').type('New book');
-    cy.get('[data-test-id="coverInput"]').type('https://bucket.mlcdn.com/a/1590/1590228/images/75a5707709691e7651c5ebcace5287da35c56015.png');
-    cy.get('[data-test-id="addButton"]').click();
+    const aBook = 'New book';
+    addBook(aBook);
     cy.get('[data-test-id="markAsReadButton"]').click();
-    cy.get('[data-test-id="titleInput"]').type('Other book');
-    cy.get('[data-test-id="coverInput"]').type('https://bucket.mlcdn.com/a/1590/1590228/images/75a5707709691e7651c5ebcace5287da35c56015.png');
-    cy.get('[data-test-id="addButton"]').click();
+    const otherBook = 'Other book';
+    addBook(otherBook);
 
     cy.get('[data-test-id="readFilterButton"]').click();
 
     cy.get('[data-test-id="bookElement"]').should('to.have.length', 1);
-    cy.get('[data-test-id="deleteButton"]').click();
+    deleteBook();
     cy.get('[data-test-id="allFilterButton"]').click();
-    cy.get('[data-test-id="deleteButton"]').click();
+    deleteBook();
   });
 
   it('should be able to update a book', ()=>{
-    cy.get('[data-test-id="titleInput"]').type('New book');
-    cy.get('[data-test-id="coverInput"]').type('https://bucket.mlcdn.com/a/1590/1590228/images/75a5707709691e7651c5ebcace5287da35c56015.png');
-    cy.get('[data-test-id="addButton"]').click();
+    const aBook = 'New book';
+    addBook(aBook);
     cy.get('[data-test-id="editButton"]').click();
     cy.get('[data-test-id="editTitleInput"]').clear().type('New book title');
 
     cy.get('[data-test-id="updateButton"]').click();
 
     cy.contains('[data-test-id="bookList"]', 'New book title').should('exist')
-    cy.get('[data-test-id="deleteButton"]').click();
+    deleteBook();
   });
 
 });
