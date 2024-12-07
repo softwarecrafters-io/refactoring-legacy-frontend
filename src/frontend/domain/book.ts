9aa748a9e1b38752545cc468d9f3aa7cf16eb483
Miguel A. Gómez <software.crafters.sl@gmail.com>
2024-12-07 17:08:28 +0000

    green: does not allow to create a book when a title length is more than the maximum

--- src/frontend/domain/book.ts
+++ src/frontend/domain/book.ts
@@ -8,6 +8,9 @@ export type Book = {
 }
 
 export function createBook(title: string, picture: string) {
+    if(title.length < 3 || title.length > 100){
+        throw new Error(`Error: The title must be between 3 and 100 characters long.`);
+    }
     return {
         id: uuid(),
         title: title,
--- src/tests/unit/domain/book.test.ts
+++ src/tests/unit/domain/book.test.ts
@@ -9,5 +9,21 @@ describe('The Book', ()=>{
         expect(book.id).toMatch(/^[a-f\d]{8}-[a-f\d]{4}-4[a-f\d]{3}-[89aAbB][a-f\d]{3}-[a-f\d]{12}$/);
         expect(book.completed).toBe(false);
     });
+
+    it('does not allow to create a book when a title length is less than the minimum', ()=>{
+       const title = 'a'.repeat(2);
+       const picture = 'http://www.example.com/book.jpg';
+
+       expect(() => createBook(title, picture))
+           .toThrowError('Error: The title must be between 3 and 100 characters long.');
+    });
+
+    it('does not allow to create a book when a title length is more than the maximum', ()=>{
+       const title = 'a'.repeat(101);
+       const picture = 'http://www.example.com/book.jpg';
+
+       expect(() => createBook(title, picture))
+           .toThrowError('Error: The title must be between 3 and 100 characters long.');
+    });
 });
 
