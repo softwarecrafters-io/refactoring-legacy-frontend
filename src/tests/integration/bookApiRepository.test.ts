c1b21ba29066ac5c11109c69767b0fd1a2a2a3c0
Miguel A. Gómez <software.crafters.sl@gmail.com>
2024-12-10 17:23:32 +0000

    green: updates a book

--- src/tests/integration/bookApiRepository.test.ts
+++ src/tests/integration/bookApiRepository.test.ts
@@ -1,9 +1,10 @@
-import {createBook} from "../../frontend/domain/book";
+import {createBook, updateTitle} from "../../frontend/domain/book";
 import {Book} from "../../backend/models/Book";
 
 type BookRepository = {
     getAll: () => Promise<Book[]>;
     add: (book: Book) => Promise<void>;
+    update: (book: Book) => Promise<void>;
     remove: (book: Book) => Promise<void>;
 }
 
@@ -22,6 +23,15 @@ function createBookApiRepository(baseUrl: string): BookRepository {
         return await response.json()
     }
 
+    const update = async (book: Book) => {
+        const response = await fetch(`${baseUrl}/${book.id}`, {
+            method: 'PUT',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify(book),
+        });
+        return await response.json()
+    }
+
     const remove = async (book: Book) => {
         await fetch(`${baseUrl}/${book.id}`, {
             method: 'DELETE'
@@ -31,6 +41,7 @@ function createBookApiRepository(baseUrl: string): BookRepository {
     return{
         getAll,
         add,
+        update,
         remove
     }
 }
@@ -47,5 +58,45 @@ describe('The Book Api Repository', ()=>{
         expect(books).toEqual([aBook]);
 
         await bookApiRepository.remove(aBook);
+    });
+
+    it('adds a book', async ()=>{
+        const baseUrl = 'http://localhost:3000/api';
+        const bookApiRepository = createBookApiRepository(baseUrl);
+        const aBook = createBook("My Book", "http://mybook.com/picture.jpg");
+
+        await bookApiRepository.add(aBook);
+
+        const books = await bookApiRepository.getAll();
+        expect(books).toEqual([aBook]);
+
+        await bookApiRepository.remove(aBook);
+    });
+
+    it('removes a book', async ()=>{
+        const baseUrl = 'http://localhost:3000/api';
+        const bookApiRepository = createBookApiRepository(baseUrl);
+        const aBook = createBook("My Book", "http://mybook.com/picture.jpg");
+
+        await bookApiRepository.add(aBook);
+        await bookApiRepository.remove(aBook);
+
+        const books = await bookApiRepository.getAll();
+        expect(books).toEqual([]);
+    });
+
+    it('updates a book', async ()=>{
+        const baseUrl = 'http://localhost:3000/api';
+        const bookApiRepository = createBookApiRepository(baseUrl);
+        const aBook = createBook("My Book", "http://mybook.com/picture.jpg");
+        await bookApiRepository.add(aBook);
+
+        const updatedBook = updateTitle(aBook, "My Updated Book");
+        await bookApiRepository.update(updatedBook);
+
+        const books = await bookApiRepository.getAll();
+        expect(books).toEqual([updatedBook]);
+
+        await bookApiRepository.remove(updatedBook);
     });
 });
