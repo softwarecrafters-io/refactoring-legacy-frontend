9b89480a89210cedb9c94648fcef28cb6854e459
Miguel A. Gómez <software.crafters.sl@gmail.com>
2024-12-11 17:17:40 +0000

    refactor: encapsulate functions in closure

--- src/frontend/application/libraryUseCase.ts
+++ src/frontend/application/libraryUseCase.ts
@@ -35,3 +35,39 @@ export const toggleToRead = async (bookRepository: BookRepository, book: Book) =
     await bookRepository.update(updatedBook)
     return updatedBook;
 };
+
+export const createLibraryUseCase = (bookRepository: BookRepository) => {
+    const getAllBooks = async (bookRepository: BookRepository) => await bookRepository.getAll();
+
+    const addBook = async (bookRepository: BookRepository, books: Book[], title: string, pictureUrl: string) => {
+        const aBook = createBook(title, pictureUrl);
+        ensureThatBookIsNotRepeated(aBook, books);
+        await bookRepository.add(aBook)
+        return aBook;
+    };
+
+    const updateBook = async (bookRepository: BookRepository, books: Book[], pictureUrl: string, book: Book, title: string) => {
+        const updatedBook = updatePicture(updateTitle(book, title), pictureUrl);
+        ensureThatBookIsNotRepeated(updatedBook, books);
+        await bookRepository.update(updatedBook)
+        return updatedBook;
+    };
+
+    const removeBook = async (bookRepository: BookRepository, book: Book) => {
+        await bookRepository.remove(book)
+    };
+
+    const toggleToRead = async (bookRepository: BookRepository, book: Book) => {
+        const updatedBook = toggleCompleted(book);
+        await bookRepository.update(updatedBook)
+        return updatedBook;
+    };
+
+    return {
+        getAllBooks,
+        addBook,
+        updateBook,
+        removeBook,
+        toggleToRead
+    }
+}
